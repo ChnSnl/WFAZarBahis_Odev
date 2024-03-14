5648762647f63a97292852f7637e7e0d7f4324d9
chnsenel@gmail.com <chnsenel@gmail.com>
2024-03-14 22:29:57 +0300

    Add project files.

---
+++ WFAZarBahis_Odev/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WFAZarBahis_Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBahis1 = new System.Windows.Forms.Button();
+            this.btnBahis2 = new System.Windows.Forms.Button();
+            this.btnZar1 = new System.Windows.Forms.Button();
+            this.btnZar2 = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblZar1 = new System.Windows.Forms.Label();
+            this.lblZar2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.lblBakiye1 = new System.Windows.Forms.Label();
+            this.lblBakiye2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBahis1
+            // 
+            this.btnBahis1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBahis1.Location = new System.Drawing.Point(156, 152);
+            this.btnBahis1.Name = "btnBahis1";
+            this.btnBahis1.Size = new System.Drawing.Size(142, 63);
+            this.btnBahis1.TabIndex = 0;
+            this.btnBahis1.Text = "Bahis Al";
+            this.btnBahis1.UseVisualStyleBackColor = true;
+            this.btnBahis1.Click += new System.EventHandler(this.btnBahis1_Click);
+            // 
+            // btnBahis2
+            // 
+            this.btnBahis2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBahis2.Location = new System.Drawing.Point(699, 152);
+            this.btnBahis2.Name = "btnBahis2";
+            this.btnBahis2.Size = new System.Drawing.Size(142, 63);
+            this.btnBahis2.TabIndex = 0;
+            this.btnBahis2.Text = "Bahis Al";
+            this.btnBahis2.UseVisualStyleBackColor = true;
+            this.btnBahis2.Click += new System.EventHandler(this.btnBahis2_Click);
+            // 
+            // btnZar1
+            // 
+            this.btnZar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnZar1.Location = new System.Drawing.Point(156, 237);
+            this.btnZar1.Name = "btnZar1";
+            this.btnZar1.Size = new System.Drawing.Size(142, 47);
+            this.btnZar1.TabIndex = 0;
+            this.btnZar1.Text = "Zar At";
+            this.btnZar1.UseVisualStyleBackColor = true;
+            this.btnZar1.Click += new System.EventHandler(this.btnZar1_Click);
+            // 
+            // btnZar2
+            // 
+            this.btnZar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnZar2.Location = new System.Drawing.Point(699, 237);
+            this.btnZar2.Name = "btnZar2";
+            this.btnZar2.Size = new System.Drawing.Size(142, 47);
+            this.btnZar2.TabIndex = 0;
+            this.btnZar2.Text = "Zar At";
+            this.btnZar2.UseVisualStyleBackColor = true;
+            this.btnZar2.Click += new System.EventHandler(this.btnZar2_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl.Location = new System.Drawing.Point(156, 46);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(142, 39);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "Kumarbaz 1";
+            this.lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(699, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kumarbaz 2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblZar1
+            // 
+            this.lblZar1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblZar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblZar1.Location = new System.Drawing.Point(151, 311);
+            this.lblZar1.Name = "lblZar1";
+            this.lblZar1.Size = new System.Drawing.Size(147, 58);
+            this.lblZar1.TabIndex = 1;
+            this.lblZar1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblZar2
+            // 
+            this.lblZar2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblZar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblZar2.Location = new System.Drawing.Point(694, 311);
+            this.lblZar2.Name = "lblZar2";
+            this.lblZar2.Size = new System.Drawing.Size(147, 58);
+            this.lblZar2.TabIndex = 1;
+            this.lblZar2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.BackColor = System.Drawing.SystemColors.Info;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(408, 105);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(194, 132);
+            this.lblSonuc.TabIndex = 1;
+            this.lblSonuc.Text = "Sonuç";
+            this.lblSonuc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBakiye1
+            // 
+            this.lblBakiye1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblBakiye1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBakiye1.Location = new System.Drawing.Point(156, 105);
+            this.lblBakiye1.Name = "lblBakiye1";
+            this.lblBakiye1.Size = new System.Drawing.Size(142, 30);
+            this.lblBakiye1.TabIndex = 1;
+            // 
+            // lblBakiye2
+            // 
+            this.lblBakiye2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblBakiye2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBakiye2.Location = new System.Drawing.Point(699, 105);
+            this.lblBakiye2.Name = "lblBakiye2";
+            this.lblBakiye2.Size = new System.Drawing.Size(142, 30);
+            this.lblBakiye2.TabIndex = 1;
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Location = new System.Drawing.Point(413, 353);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(189, 58);
+            this.btnKapat.TabIndex = 3;
+            this.btnKapat.Text = "Çıkış";
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1218, 728);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblZar2);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.lblBakiye2);
+            this.Controls.Add(this.lblBakiye1);
+            this.Controls.Add(this.lblZar1);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.btnZar2);
+            this.Controls.Add(this.btnZar1);
+            this.Controls.Add(this.btnBahis2);
+            this.Controls.Add(this.btnBahis1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBahis1;
+        private System.Windows.Forms.Button btnBahis2;
+        private System.Windows.Forms.Button btnZar1;
+        private System.Windows.Forms.Button btnZar2;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblZar1;
+        private System.Windows.Forms.Label lblZar2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Label lblBakiye1;
+        private System.Windows.Forms.Label lblBakiye2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnKapat;
+    }
+}
+
---
+++ WFAZarBahis_Odev/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAZarBahis_Odev
+{
+    //Form basladıgında 2. oyuncunun bahis yatırma secenegi kapalı olsun...
+    //Aynı zamanda zar atma butonları da kapalı olsun...
+    //1. oyuncu bahis yatırdıgında birinci oyuncunun bahis yatırma butonu deaktif olsun
+    //2. oyuncunun bahis yatırma butonu acılsın...(bahisler ancak 100 lira yatırılabilsin...)
+    //İkinci oyuncu da bahsi yatırdıktan sonra onun da bahis yatırma butonu deaktif olsun ve 1. oyuncunun zar atma butonu acılsın...
+    //1. Oyuncu zar attıktan sonra zar sonucunu ilgili label'ina yazdırın sonra 1. oyuncunun zar atma butonu kapansın ve 2. oyuncunun zar atma butonu acılsın...
+    //2. oyuncu zar atsın ve onun da sonucunu yazdırın ve kimin kazandıgını belirleyin...
+    //Oyuncular berabere zar atarsa o ilgili zar atısı tekrarlansın...
+    //Yani 2. oyuncunun zar atısını kapatın 1. oyuncununkini acın sonra 2. oyuncununkini acarsınız...
+    //Birisi kazanırsa tekrar 1. oyuncunun bahis butonu acılsın diger butonlar kapansın ve bahis sisteminden oyun yeniden baslasın...
+    //Bir oyuncu bahis icin 100 lira yatıramayacak konuma geldigi an oyun diger oyuncunun zaferiyle sonlansın... 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        Random zar = new Random();
+        int bakiye1, bakiye2, bahis;
+       
+        private void btnBahis1_Click(object sender, EventArgs e)
+        {
+            bakiye1 -= bahis;
+            lblBakiye1.Text = bakiye1.ToString();
+            btnBahis1.Enabled = false;
+            btnBahis2.Enabled = true;
+                           
+        }
+
+        private void btnBahis2_Click(object sender, EventArgs e)
+        {
+            bakiye2 -= bahis;
+            lblBakiye2.Text = bakiye2.ToString();
+            btnBahis2.Enabled = false;
+            btnZar1.Enabled = true;
+         
+        }
+        private void btnZar1_Click(object sender, EventArgs e)
+        {
+            //lblZar1.Text = Convert.ToString(zar.Next(1, 10));
+            lblZar1.Text = zar.Next(1, 7).ToString();
+            btnZar2.Enabled = true;
+            btnZar1.Enabled = false;
+        }
+
+        private void btnZar2_Click(object sender, EventArgs e)
+        {
+            lblZar2.Text = zar.Next(1, 7).ToString();
+            btnZar2.Enabled = false;
+
+            if (lblZar1.Text == lblZar2.Text)
+            {
+                lblSonuc.Text = "Kazanan çıkmadı.\nZar atışı tekrarlansın.";
+                btnZar1.Enabled=true;
+            }
+            else if (Convert.ToInt32(lblZar1.Text) > Convert.ToInt32(lblZar2.Text))
+            {
+                lblSonuc.Text = "1.Kumarbaz Kazandı.\nBahisler açılmıştır.";
+                lblBakiye1.Text = (bakiye1 += 200).ToString();
+                btnBahis1.Enabled = true;
+                
+            }
+            else if (Convert.ToInt32(lblZar1.Text) < Convert.ToInt32(lblZar2.Text)) 
+            {
+                lblSonuc.Text = "2.Kumarbaz Kazandı.\nBahisler açılmıştır.";
+                lblBakiye2.Text = (bakiye2 += 200).ToString();
+                btnBahis1.Enabled = true;
+            }
+
+
+            if(Convert.ToInt32(lblBakiye1.Text) <= 0 || Convert.ToInt32(lblBakiye2.Text) <= 0) 
+            { 
+                btnBahis1.Enabled=false;
+                MessageBox.Show("Bakiye yetersiz.");
+                Application.Exit();
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnBahis2.Enabled = false;
+            btnZar2.Enabled = false;
+            btnZar1.Enabled = false;
+            bakiye1 = 300; bakiye2 = 300; bahis = 100;
+            lblBakiye1.Text = Convert.ToString(bakiye1);
+            lblBakiye2.Text = Convert.ToString(bakiye2);
+
+
+            timer1.Start();
+
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
